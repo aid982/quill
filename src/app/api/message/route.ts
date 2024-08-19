8fb64ae9fee22a41551d231f9eb28c9936cb8f14
Volodymyr <vovoset82@gmail.com>
2024-08-19 16:48:41 +0300

    Gemini init

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -8,7 +8,11 @@ import { and, eq, sql } from "drizzle-orm";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { NextRequest } from "next/server";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { OpenAIStream, StreamingTextResponse, GoogleGenerativeAIStream } from "ai";
+
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { tr } from "date-fns/locale";
 
 export const POST = async (req: NextRequest) => {
   // api for asking question to a PDF
@@ -47,75 +51,23 @@ export const POST = async (req: NextRequest) => {
         isUserMessage: true,
       })
       .returning();
-    console.log(insertedData);
+
   } catch (error) {
     console.log(error);
     return new Response("SERVER ERROR", { status: 500 });
   }
-
-  // Vectorize
-  const embaddings = new OpenAIEmbeddings({
-    openAIApiKey: process.env.OPEN_AI,
-  });
-  console.log(embaddings);
-
-  const pineconeIndex = pinecone.Index("quill");
-
-  const vectorStore = await PineconeStore.fromExistingIndex(embaddings, {
-    pineconeIndex,
-  });
-
-  console.log(vectorStore);
-
-  const results = await vectorStore.similaritySearch(message, 2);
-  console.log(message)
-  console.log(results);  
-
-  const formattedPrevMessages = prevMessages.map((msg) => ({
-    role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
-    content: msg.text,
-  }));
-
-  const withoutContext = false;
-
-  const openAIMessage = [
-    {
-      role: "system" as const,
-      content:
-        "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
-    },
-    {
-      role: "user" as const,
-      content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
-      
-\n----------------\n
-
-PREVIOUS CONVERSATION:
-${formattedPrevMessages.map((message) => {
-  if (message.role === "user") return `User: ${message.content}\n`;
-  return `Assistant: ${message.content}\n`;
-})}
-
-\n----------------\n
-${withoutContext ? '':`CONTEXT:
-${results.map((r) => r.pageContent).join("\n\n")}`}
-
-USER INPUT: ${message}`,
-    },
-  ];
-
-  console.log(openAIMessage);
-
   try {
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      temperature: 0,
-      stream: true,
-      messages: openAIMessage,
-    });
-    const stream = OpenAIStream(response, {
+
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY!);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+
+    const response = await model.generateContentStream(message);
+
+    const stream = GoogleGenerativeAIStream(response, {
       async onCompletion(completion) {
-        console.log(completion)
+        console.log('Complition',completion)
         await db.insert(messages).values({
           text: completion,
           isUserMessage: false,
@@ -124,13 +76,14 @@ USER INPUT: ${message}`,
         });
       },
     });
+
     return new StreamingTextResponse(stream);
-    
+
   } catch (error) {
     console.log(error)
     return new Response("SERVER ERROR", { status: 500 });
-    
+
   }
 
-  
+
 };
